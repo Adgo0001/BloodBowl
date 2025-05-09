3878445f6db034b4d7df9a0e45daf7993301c443
Adrian <adgo0001@stud.kea.dk>
2025-05-09 13:33:15 +0200

    Html sider lavet

--- src/main/java/com/example/bloodbowl/BloodBowlApplication.java
+++ src/main/java/com/example/bloodbowl/BloodBowlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BloodBowlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloodBowlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BloodBowlApplication.class, args);
+    }
 
 }
