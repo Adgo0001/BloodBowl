ecf977fe0bf837692727386c9399bf51e46c509c
Adrian <adgo0001@stud.kea.dk>
2025-05-09 13:24:06 +0200

    Initial commit

---
+++ src/test/java/com/example/bloodbowl/BloodBowlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloodbowl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBowlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
